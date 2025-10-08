108e8c4deb2c57b21fc1ad70119b60ae5a056225
ghost-mann <iamaob8@protonmail.com>
2025-10-08 12:15:34 +0300

    feat: preparing selenium robot function in main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import re
 from pymongo import MongoClient
 from selenium import webdriver
 
+def setup_driver():
+    options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": DOWNLOAD_DIR}
+    options.add_experimental_option("prefs",prefs)
+    driver = webdriver.Chrome(options=options)
+    return driver
